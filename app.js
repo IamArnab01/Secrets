e290dd5e7f0a7cc3e23daabfb61d7c56a2cf3361
Your Name <IamArnab01>
2019-10-24 14:57:20 +0530

    environmrntal variables added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config(); // to secure all passwrod fields
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -11,11 +13,12 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB",{ useNewUrlParser: true ,useUnifiedTopology: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email:String,
   password:String
-};
-
+});
+// ------ for encryption of password-----
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 const User = new mongoose.model("User",userSchema);
 
 // const user1 = new User({
@@ -44,6 +47,11 @@ app.get("/",function(req,res){
 app.get("/login",function(req,res){
   res.render("login");
 });
+
+// redirects to home page after logout
+app.get("/logout",function(req,res){
+  res.redirect("/");
+})
 
 app.get("/register",function(req,res){
   res.render("register");
